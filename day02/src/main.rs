3abb9718df53a7b990b588f2faf9cb3bd2b53676
Dave Reed <dave.reed@alteryx.com>
2022-12-03 15:20:40 -0500

    day03 done

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -31,4 +31,22 @@ fn main() {
     }
 
     println!("Part 1: {:?}", score);
+
+    let mut score = 0;
+    for game in &games {
+        match game {
+            ["A", "X"] => score += 3 + 0,
+            ["A", "Y"] => score += 1 + 3,
+            ["A", "Z"] => score += 2 + 6,
+            ["B", "X"] => score += 1 + 0,
+            ["B", "Y"] => score += 2 + 3,
+            ["B", "Z"] => score += 3 + 6,
+            ["C", "X"] => score += 2 + 0,
+            ["C", "Y"] => score += 3 + 3,
+            ["C", "Z"] => score += 1 + 6,
+            _ => println!("nope"),
+        }
+    }
+
+    println!("Part 2: {:?}", score);
 }
--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,64 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+
+    let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let sacks = input
+        .split("\n")
+        .filter(|&s| s != "")
+        .map(|s| {
+            let len = s.len();
+            let a = &s[..len / 2];
+            let b = &s[len / 2..];
+            // let mut sack = vec![];
+
+            return [a, b];
+        })
+        .collect::<Vec<[&str; 2]>>();
+
+    let mut sum = 0;
+    'outer: for sack in &sacks {
+        let pocket1 = sack[0];
+        let pocket2 = sack[1];
+
+        for c in pocket1.chars() {
+            if pocket2.contains(c) {
+                let idx = alphabet.chars().position(|c1| c1 == c).unwrap() + 1;
+                // println!("{} {}-> {} {}", c, idx, pocket1, pocket2);
+                sum += idx;
+
+                continue 'outer;
+            }
+        }
+    }
+    println!("Part 1: {:?}", sum);
+
+    let sacks: Vec<&str> = input
+        .split("\n")
+        .filter(|&s| s != "")
+        .collect::<Vec<&str>>();
+
+    let sacks3: Vec<&[&str]> = sacks.chunks(3).collect();
+
+    let mut sum = 0;
+    'outer: for sack in sacks3 {
+        let a = sack[0];
+        let b = sack[1];
+        let c = sack[2];
+        println!("{} {} {}", a, b, c);
+
+        for d in a.chars() {
+            if b.contains(d) && c.contains(d) {
+                let idx = alphabet.chars().position(|c1| c1 == d).unwrap() + 1;
+                // println!("{} {}-> {} {}", c, idx, pocket1, pocket2);
+                sum += idx;
+
+                continue 'outer;
+            }
+        }
+    }
+
+    println!("Part 2: {:?}", sum);
+
+    // println!("{:?}", a);
 }
