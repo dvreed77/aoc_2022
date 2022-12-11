2c5104d7428098854d24e18ce1d93d721e9facaa
Dave Reed <david.reed.c@gmail.com>
2022-12-11 16:15:30 +0100

    cleaned up day11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,53 +1,47 @@
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_till, take_until},
-    character::complete::{self, alphanumeric1, newline, none_of, not_line_ending},
-    combinator::{complete, eof, map, opt},
-    multi::{fold_many0, fold_many1, separated_list1},
-    sequence::{pair, preceded, separated_pair, tuple},
+    bytes::complete::tag,
+    character::complete::{self, newline},
+    combinator::map,
+    multi::separated_list1,
+    sequence::{preceded, separated_pair, tuple},
     *,
 };
-use num::BigInt;
 use std::collections::{BTreeMap, VecDeque};
 
 #[derive(Debug)]
 enum Operation {
-    Add(u32),
-    Multiply(u32),
+    Add(u64),
+    Multiply(u64),
     Square,
 }
 
 #[derive(Debug)]
 struct Monkey {
-    num: u32,
-    items: VecDeque<BigInt>,
+    items: VecDeque<u64>,
     op: Operation,
-    t: u32,
-    tf: (u32, u32),
+    t: u64,
+    tf: (u64, u64),
 }
 
-fn item_parser(i: &str) -> IResult<&str, VecDeque<BigInt>> {
+fn item_parser(i: &str) -> IResult<&str, VecDeque<u64>> {
     // Starting items: 79, 98
     let (input, v) = preceded(
         tag("  Starting items: "),
-        separated_list1(tag(", "), complete::u32),
+        separated_list1(tag(", "), complete::u64),
     )(i)?;
 
-    let v2: VecDeque<u32> = v.into();
-    let v3 = v2
-        .iter()
-        .map(|&x| BigInt::from(x))
-        .collect::<VecDeque<BigInt>>();
+    let v2: VecDeque<u64> = v.into();
 
-    return Ok((input, v3));
+    return Ok((input, v2));
 }
 
 fn operation2_parser(i: &str) -> nom::IResult<&str, Operation> {
     alt((
-        map(preceded(tag("old * "), complete::u32), |x| {
+        map(preceded(tag("old * "), complete::u64), |x| {
             Operation::Multiply(x)
         }),
-        map(preceded(tag("old + "), complete::u32), |x| {
+        map(preceded(tag("old + "), complete::u64), |x| {
             Operation::Add(x)
         }),
         map(tag("old * old"), |_| Operation::Square),
@@ -59,26 +53,23 @@ fn operation_parser(i: &str) -> IResult<&str, Operation> {
     preceded(tag("  Operation: new = "), operation2_parser)(i)
 }
 
-fn test_parser(i: &str) -> IResult<&str, u32> {
+fn test_parser(i: &str) -> IResult<&str, u64> {
     // Test: divisible by 23
-    preceded(tag("  Test: divisible by "), complete::u32)(i)
+    preceded(tag("  Test: divisible by "), complete::u64)(i)
 }
 
-fn tf_parser(i: &str) -> IResult<&str, (u32, u32)> {
+fn tf_parser(i: &str) -> IResult<&str, (u64, u64)> {
     // If true: throw to monkey 2
     // If false: throw to monkey 3
     separated_pair(
-        preceded(tag("    If true: throw to monkey "), complete::u32),
+        preceded(tag("    If true: throw to monkey "), complete::u64),
         newline,
-        preceded(tag("    If false: throw to monkey "), complete::u32),
+        preceded(tag("    If false: throw to monkey "), complete::u64),
     )(i)
-}
-fn part1(input: &str) {
-    println!("Part 1: {}", input);
 }
 
 fn monkey_parser(i: &str) -> IResult<&str, Monkey> {
-    let (input, (_, num, _)) = tuple((tag("Monkey "), complete::u32, tag(":\n")))(i)?;
+    let (input, (_, _, _)) = tuple((tag("Monkey "), complete::u64, tag(":\n")))(i)?;
 
     let (input, items) = item_parser(input)?;
 
@@ -89,16 +80,7 @@ fn monkey_parser(i: &str) -> IResult<&str, Monkey> {
     let (input, _) = newline(input)?;
     let (input, tf) = tf_parser(input)?;
 
-    Ok((
-        input,
-        Monkey {
-            num,
-            items,
-            op,
-            t,
-            tf,
-        },
-    ))
+    Ok((input, Monkey { items, op, t, tf }))
 }
 
 fn parser(i: &str) -> IResult<&str, Vec<Monkey>> {
@@ -111,92 +93,86 @@ fn main() {
 
     let mut monkeys = parser(input).unwrap().1;
 
-    let mut my_map: BTreeMap<u32, BigInt> = BTreeMap::new();
+    let mut my_map: BTreeMap<u64, u64> = BTreeMap::new();
 
     for round in 0..20 {
         for idx in 0..monkeys.len() {
-            let il = monkeys[idx].items.len() as u32;
+            let il = monkeys[idx].items.len() as u64;
             my_map
-                .entry(idx as u32)
+                .entry(idx as u64)
                 .and_modify(|x| *x += il)
-                .or_insert(BigInt::from(il));
+                .or_insert(il);
             for _ in 0..monkeys[idx].items.len() {
                 let i = monkeys[idx].items.pop_front().unwrap().clone();
                 let j = i.clone();
                 // dbg!(i);
-                let w = match monkeys[idx].op {
-                    Operation::Add(x) => i + BigInt::from(x),
-                    Operation::Multiply(x) => i * BigInt::from(x),
-                    Operation::Square => i * j,
+                let mut w = match monkeys[idx].op {
+                    Operation::Add(x) => i + x,
+                    Operation::Multiply(x) => i * x,
+                    Operation::Square => i * i,
                 }
                 .clone();
                 // dbg!(w);
-                let w1: BigInt = w.clone() / 3;
+                w = w / 3;
                 // dbg!(w);
                 let tf = monkeys[idx].tf;
-                if &w1 % monkeys[idx].t == BigInt::from(0) {
-                    monkeys[tf.0 as usize].items.push_back(w1.clone());
+                if &w % monkeys[idx].t == 0 {
+                    monkeys[tf.0 as usize].items.push_back(w);
                 } else {
-                    monkeys[tf.1 as usize].items.push_back(w1);
+                    monkeys[tf.1 as usize].items.push_back(w);
                 }
             }
         }
     }
 
-    let mut v = my_map
-        .iter()
-        .map(|(&k, v)| v.clone())
-        .collect::<Vec<BigInt>>();
+    let mut v = my_map.iter().map(|(&k, v)| v.clone()).collect::<Vec<u64>>();
 
     v.sort_by(|a, b| b.cmp(a));
 
-    dbg!(v[0].clone() * v[1].clone());
+    dbg!(v[0] * v[1]);
 
     let mut monkeys = parser(input).unwrap().1;
 
-    let d = monkeys.iter().map(|m| m.t).product::<u32>();
+    let d = monkeys.iter().map(|m| m.t).product::<u64>();
 
     dbg!(d);
 
-    let mut my_map: BTreeMap<u32, BigInt> = BTreeMap::new();
+    let mut my_map: BTreeMap<u64, u64> = BTreeMap::new();
 
     for _ in 0..10_000 {
         for idx in 0..monkeys.len() {
-            let il = monkeys[idx].items.len() as u32;
+            let il = monkeys[idx].items.len() as u64;
             my_map
-                .entry(idx as u32)
+                .entry(idx as u64)
                 .and_modify(|x| *x += il)
-                .or_insert(BigInt::from(il));
+                .or_insert(il);
             for _ in 0..monkeys[idx].items.len() {
                 let i = monkeys[idx].items.pop_front().unwrap().clone();
                 let j = i.clone();
                 // dbg!(i);
-                let w = match monkeys[idx].op {
-                    Operation::Add(x) => i + BigInt::from(x),
-                    Operation::Multiply(x) => i * BigInt::from(x),
+                let mut w = match monkeys[idx].op {
+                    Operation::Add(x) => i + x,
+                    Operation::Multiply(x) => i * x,
                     Operation::Square => (i * j),
                 }
                 .clone();
-                // dbg!(w);
-                let w1: BigInt = w.clone() % d;
-                // dbg!(w);
+
+                w = w % d;
+
                 let tf = monkeys[idx].tf;
-                if &w1 % monkeys[idx].t == BigInt::from(0) {
-                    monkeys[tf.0 as usize].items.push_back(w1.clone());
+                if &w % monkeys[idx].t == 0 {
+                    monkeys[tf.0 as usize].items.push_back(w);
                 } else {
-                    monkeys[tf.1 as usize].items.push_back(w1);
+                    monkeys[tf.1 as usize].items.push_back(w);
                 }
             }
         }
     }
 
-    let mut v = my_map
-        .iter()
-        .map(|(&k, v)| v.clone())
-        .collect::<Vec<BigInt>>();
+    let mut v = my_map.iter().map(|(&k, v)| v.clone()).collect::<Vec<u64>>();
 
     v.sort_by(|a, b| b.cmp(a));
 
     dbg!(my_map);
-    dbg!(v[0].clone() * v[1].clone());
+    dbg!(v[0] * v[1]);
 }
